58106275ba9888c0e96be23205634fe9845e8333
avian5 <avian883@gmail.com>
2026-07-06 10:27:15 -0700

    Add glider controller files

---
+++ esp32_glider_controller/.pio/build/esp32-s3-devkitm-1/FrameworkArduino/cbuf.cpp.d
@@ -0,0 +1,3 @@
+.pio/build/esp32-s3-devkitm-1/FrameworkArduino/cbuf.cpp.o: \
+ /Users/avian/.platformio/packages/framework-arduinoespressif32/cores/esp32/cbuf.cpp \
+ /Users/avian/.platformio/packages/framework-arduinoespressif32/cores/esp32/cbuf.h
---
+++ esp32_glider_controller/.pio/build/esp32-s3-devkitm-1/lib687/WebServer/detail/mimetable.cpp.d
@@ -0,0 +1,4 @@
+.pio/build/esp32-s3-devkitm-1/lib687/WebServer/detail/mimetable.cpp.o: \
+ /Users/avian/.platformio/packages/framework-arduinoespressif32/libraries/WebServer/src/detail/mimetable.cpp \
+ /Users/avian/.platformio/packages/framework-arduinoespressif32/libraries/WebServer/src/detail/mimetable.h \
+ /Users/avian/.platformio/packages/framework-arduinoespressif32/cores/esp32/pgmspace.h
